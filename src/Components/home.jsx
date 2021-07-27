5835832c563dc17b148689ae01de0825f743f830
JosephPR <reilly4181@gmail.com>
2021-07-27 17:49:37 -0600

    updated homepage

--- src/Components/home.jsx
+++ src/Components/home.jsx
@@ -1,16 +1,19 @@
 import React from 'react';
 
-import gif from './board.gif'
+// import gif from './board.gif'
 import '../App.css';
 
 function Home() {
   return (
     <>
     <div className="hero">
-    <h2>Here the student becomes the master</h2>
+    <h2>This is where the student<br/> becomes the master</h2>
+    <p>A deep dive into learning and growing as a chess player</p>
 
     </div>
+
     {/* <img className="board" src={gif} alt="board-gif"/> */}
+
     </>
   );
 }
